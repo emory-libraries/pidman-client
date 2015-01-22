1b071d2301c200822427df874f133c574b6bfe53
rsutton <rsutton@79750155-2c60-425a-961e-bef42b9dd43f>
2015-01-22 22:27:17 +0000

    create 1.2.x release branch

---
+++ pidmanclient/branches/release-1.2.x/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+import pidservices
+
+setup(
+    name='pidservices',
+    version=pidservices.__version__,
+    author=pidservices.__author__,
+    author_email=pidservices.__email__,
+    packages=['pidservices', 'pidservices.djangowrapper'],
+    install_requires=[
+        'requests',
+    ],
+    scripts=['scripts/allocate_pids',],
+)
